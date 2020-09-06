615b4d6be3043014657d08c7a80e5eda456d9664
syntaxxxxx <fiqrihafzainislami@gmail.com>
2020-09-06 15:30:41 +0700

    starter project of webinar niomic

--- app/src/main/java/com/langitpayment/githubapi/MainActivity.kt
+++ app/src/main/java/com/langitpayment/githubapi/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.langitpayment.githubapi
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        val customToolbar = layoutInflater.inflate(R.layout.custom_toolbar, null)
+        supportActionBar?.customView = customToolbar
     }
 }
